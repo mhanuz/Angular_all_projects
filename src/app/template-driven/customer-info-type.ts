952d010abcb08c85b62c95b8c487fca5dbe7b06b
Anuz <ceanuz66@gmail.com>
2023-02-14 16:59:16 +0600

    add indentation

--- src/app/template-driven/customer-info-type.ts
+++ src/app/template-driven/customer-info-type.ts
@@ -1,31 +1,31 @@
 export interface CustomerInfo {
-    basicInfo: BasicInfo;
-    primaryAddress: PrimaryAddress;
-    secondaryAddress: SecondaryAddress;
-  }
+  basicInfo: BasicInfo;
+  primaryAddress: PrimaryAddress;
+  secondaryAddress: SecondaryAddress;
+}
   
 export interface  BasicInfo{
-      firstName: string;
-      lastName: string;
-      email?: string;
-      phone: number;
-    }
-  
+  firstName: string;
+  lastName: string;
+  email?: string;
+  phone: number;
+}
+
 export  interface  PrimaryAddress{
-      email?: string;
-      phone: number;
-      address: string;
-      city: string;
-      country: string;
-      postCode: string;
-    }
+  email?: string;
+  phone: number;
+  address: string;
+  city: string;
+  country: string;
+  postCode: string;
+}
   
 export  interface SecondaryAddress{
-      email?: string;
-      phone: number;
-      address: string;
-      city: string;
-      country: string;
-      postCode: string; 
-    }
+  email?: string;
+  phone: number;
+  address: string;
+  city: string;
+  country: string;
+  postCode: string; 
+}
   
